4dfdea2db4802ca9a636b0db42861a0ae36b3a6d
Kimberly <kimberly@Administrators-MacBook-Pro.local>
2021-12-09 12:36:30 -0500

    Updates

---
+++ component---src-pages-townhouse-jsx-f5d1a2a6ca89de49922d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[608],{9448:function(e,t,l){l.r(t);var n=l(7294),a=l(2341),r=l(1277),u=l(8953),c=l(7876),s=l(6228),f=l(3055);t.default=function(){return n.createElement(a.Z,null,n.createElement(r.Z,{title:"Living Little"}),n.createElement(u.Z,null),n.createElement(f.Z,null),n.createElement(c.Z,{typology:"townhouse"}),n.createElement(s.Z,null))}}}]);
+//# sourceMappingURL=component---src-pages-townhouse-jsx-f5d1a2a6ca89de49922d.js.map
