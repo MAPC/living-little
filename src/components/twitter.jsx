b9490e92675505ae2c63dbcd0d28080b15c09e12
Kimberly <kimberly@AdminisatorsMBP.attlocal.net>
2021-12-20 11:23:45 -0500

    Add definitins to tweets

--- src/components/twitter.jsx
+++ src/components/twitter.jsx
@@ -5,6 +5,16 @@ import ShareLink from 'react-twitter-share-link';
 
 const Twitter = ({typology}) => {
   
+  const tweetText = {
+    adu: "A smaller apartment on the same lot as a larger main house.",
+    cottage: "A pedestrian-friendly cluster of one- to two-story houses with shared green spaces.",
+    tinyhouse: "A house smaller than 500 square feet, on wheels or a foundation, usually with a high-pitched ceiling and a porch.",
+    townhouse: "A narrow, multi-level home with an individual entry that shares a common side wall(s) with another dwelling.",
+    sfc: "A single-family house that has been converted into two or more housing units, typically with few or no changes to the exterior.",
+    mixeduse: "One to three floors of housing located above ground floor retail with parking located behind or below the building.",
+    cohousing: "An intentionally-designed community of private residences and shared public open and common spaces. Cohousing can take many forms, including single family, duplexes, townhouses, cottages, or a combination of several housing types."
+  }
+
   const deepDiveUrl = {
     adu: "https://mapc.github.io/living-little/adu#select",
     cottage: "https://mapc.github.io/living-little/cottage/#select",
@@ -16,7 +26,7 @@ const Twitter = ({typology}) => {
   };
   
   return (
-    <ShareLink link={deepDiveUrl[typology]}>
+    <ShareLink text={tweetText[typology]} link={deepDiveUrl[typology]}>
       {link => (
           <a href={link} target='_blank'>    
             <FontAwesomeIcon icon={faTwitter} className="social-sharing__icon social-sharing__icon--twitter" />
