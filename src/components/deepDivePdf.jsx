bc65c624abb3f88d5ad1a5a2dab03c62d644c3a6
Kimberly <kimberly@Administrators-MacBook-Pro.local>
2022-01-11 14:36:18 -0500

    Add citations

--- src/components/deepDivePdf.jsx
+++ src/components/deepDivePdf.jsx
@@ -3,9 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPrint } from '@fortawesome/free-solid-svg-icons';
 import aduPdf from "../content/SS_LL_Report_ADU.pdf";
 import cottagePdf from "../content/SS_LL_Report_Cottage.pdf";
-import townhousePdf from "../content/SS_LL_Report_Townhouse.pdf";
-import sfcPdf from "../content/SS_LL_Report_SFHC.pdf";
-import mixedusePdf from "../content/SS_LL_Report_Small_Scale_Mixed_Use.pdf";
+import townhousePdf from "../content/Townhouse.pdf";
+import sfcPdf from "../content/Conversion.pdf";
+import mixedusePdf from "../content/Mixed-use.pdf";
 import cohousingPdf from "../content/SS_LL_Report_Cohousing.pdf";
 
 const DeepDivePdf = ({typology}) => {
