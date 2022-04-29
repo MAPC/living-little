d1356db169c48a617594bf6253402b78458eb6dd
Kimberly <kimberly@adminisatorsmbp.mynetworksettings.com>
2022-04-29 11:13:06 -0400

    Clean up popup from Adu page for deployment

--- src/pages/adu.jsx
+++ src/pages/adu.jsx
@@ -6,20 +6,13 @@ import DeepDive from '../components/deepDive';
 import LearnMore from '../components/learnMore';
 import ContextVideos from '../components/contextVideos';
 import { PopupProvider } from '../components/contexts/popupContext';
-import { PopupContext } from '../components/contexts/popupContext';
-import Popup from '../components/modules/popup';
 
 const Adu = () => {
-  
-  const { popupState, sourceState } = useContext(PopupContext)
-  const [popup, togglePopup] = popupState;
-  const [popupSource, setPopupSource] = sourceState;
   
   return (
     <Layout>
       <PopupProvider>
         <SEO title="Living Little" />
-        {/* <Popup /> */}
         <TypologyGrid />
         <ContextVideos />
         <DeepDive typology="adu" />
